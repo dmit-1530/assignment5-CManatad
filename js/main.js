bdf383a65c8d037c1b619a4976c81a2ea8a31c46
CManatad <clarencemanatad25@gmail.com>
2026-03-27 20:33:27 -0600

    Change and have a trial and error

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-document.querySelector('.toggle-menu').addEventListener('click', () => {
+document.querySelector('.menu-container').addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('show-menu');
 });
